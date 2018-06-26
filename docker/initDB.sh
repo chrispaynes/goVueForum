d08ed567be81ef4733eda03755f70ea83dfee883
chrispaynes <cdpaynes@gmail.com>
2018-06-25 21:41:17 -0600

    updates DB schemas

--- docker/initDB.sh
+++ docker/initDB.sh
@@ -9,12 +9,20 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-S
       title                   VARCHAR(50) NOT NULL UNIQUE
   );
 
+  INSERT INTO forum_category(title)
+  VALUES  ('Test Category 1'),
+          ('Test Category 2');
+
   CREATE TABLE IF NOT EXISTS forum (
-      forum_id      SERIAL PRIMARY KEY,
-      category      INTEGER REFERENCES forum_category(forum_category_id) ON DELETE CASCADE,
-      title         VARCHAR(50) NOT NULL UNIQUE,
-      description   VARCHAR(250) NOT NULL
+      forum_id               SERIAL PRIMARY KEY,
+      forum_category_id      INTEGER REFERENCES forum_category(forum_category_id) ON DELETE CASCADE,
+      title                  VARCHAR(50) NOT NULL UNIQUE,
+      description            VARCHAR(250) NOT NULL
   );
+
+  INSERT INTO forum(category, title, description)
+  VALUES  (1, 'Test Forum 1', 'Test 1 Description'),
+          (2, 'Test Forum 2', 'Test 1 Description');
 
   CREATE TABLE IF NOT EXISTS user_account (
       user_account_id     SERIAL PRIMARY KEY,
@@ -44,7 +52,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-S
   CREATE TABLE IF NOT EXISTS post (
       post_id             SERIAL PRIMARY KEY,
       author_id           INTEGER REFERENCES user_account(user_account_id) ON DELETE CASCADE,
-      title               VARCHAR(25) NOT NULL,
+      thread_id           INTEGER REFERENCES thread(thread_id) ON DELETE CASCADE,
+      title               VARCHAR(25),
       body                INTEGER REFERENCES post_body(post_body_id) ON DELETE CASCADE,
       created_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
       last_updated_at     TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -53,6 +62,19 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-S
   INSERT INTO post(author_id, title, body)
   VALUES  (1, 'Test Article 1', 1),
           (2, 'Test Article 2', 2);
+
+  CREATE TABLE IF NOT EXISTS thread (
+    thread_id           SERIAL PRIMARY KEY,
+    forum_id            INTEGER REFERENCES forum(forum_id) ON DELETE CASCADE,
+    title               VARCHAR(100) NOT NULL,
+    author_id           INTEGER REFERENCES user_account(user_account_id) ON DELETE CASCADE,
+    created_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    last_reply_at       TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  );
+
+  INSERT INTO thread(forum_id, title, author_id)
+  VALUES  (1, 'Test Thread 1', 1),
+          (2, 'Test Thread 2', 2);
 
   CREATE VIEW posts_v AS
     SELECT p.post_id, ua.username, p.author_id, p.title, p.created_at, p.last_updated_at, pb.body
